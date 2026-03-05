4398b8c1991dc941e0b3b7d488c4647db6f95367
Andy-djc <1776080938@qq.com>
2026-03-05 17:50:13 +0800

    Update Main.java

--- src/main/java/net/xdclass/Main.java
+++ src/main/java/net/xdclass/Main.java
@@ -21,7 +21,10 @@ public class Main {
         System.out.println("hot-fox test3");
         System.out.println("hot-fox test4");
         System.out.println("hot-fox test5");
-
+        System.out.println("hot-fox test6");
+        
+        
+        
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
